70ab6181eb57cb1805c3216a9fcb9273f9c2e998
Gabriel <discenzagabriel41@gmail.com>
2026-04-14 22:55:18 -0300

    Cambio en DB.js

--- config/db.js
+++ config/db.js
@@ -3,17 +3,18 @@ const mysql = require('mysql2');
 
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT, // 👈 importante
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
 });
+
 db.connect(err => {
     if (err) {
         console.error('Error de conexión:', err);
         return;
     }
-    console.log('Conectado a MySQL');
+    console.log('Conectado a Railway 🚀');
 });
-console.log(process.env.DB_USER);
-console.log(process.env.DB_PASSWORD);
+
 module.exports = db;
