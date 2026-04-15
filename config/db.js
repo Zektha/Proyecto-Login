1b5e004bbdc2142d2c56e70cef0998102640bb70
Gabriel <discenzagabriel41@gmail.com>
2026-04-14 23:04:22 -0300

    Cambio en db2

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 const mysql = require('mysql2');
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT, // 👈 importante
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
