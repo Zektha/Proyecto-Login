3ae0132a02d31c3d102a373da00b7f9575c98c0b
Gabriel <discenzagabriel41@gmail.com>
2026-04-14 22:46:01 -0300

    Añadiendo Cors

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const session = require('express-session');
 const authRoutes = require('./routes/authRoutes');
+const cors = require('cors');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(session({
